18517bdf112505453c5d753ea8cfabc9d45401d7
Darkangelike <72765580+Darkangelike@users.noreply.github.com>
2020-10-27 14:50:30 +0100

    initial

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itieu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 17:07:41 by itieu             #+#    #+#             */
+/*   Updated: 2020/10/27 13:23:16 by itieu            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	if (size == 0)
+		return (i);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
